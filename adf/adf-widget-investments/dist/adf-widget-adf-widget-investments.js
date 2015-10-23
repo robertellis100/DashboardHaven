a382a4638d6b731a0ed44533d17cb17a9e8609c7
Robert Ellis <robertellis.rob@gmail.com>
2015-10-22 23:24:44 -0600

    Investment Widget: 1) "Loading Bar" works with promise on single API call 2)moved data entry of ticker to "edit.html" from "view.html"

--- adf/adf-widget-investments/dist/adf-widget-adf-widget-investments.js
+++ adf/adf-widget-investments/dist/adf-widget-adf-widget-investments.js
@@ -4,29 +4,47 @@ var app = angular.module('adf.widget.investments', ['adf.provider'])
     dashboardProvider
       .widget('investments', {
         title: 'Investment Analyzer',
-        description: 'Review your business....Fill this in later',
+        description: 'Current financial information on public companies',
         templateUrl: '{widgetsPath}/adf-widget-investments/src/view.html',
+        controller: 'RequestController',
+        reload: true,
+        resolve: {
+          data: ["RequestService", "config", function(RequestService, config){
+            if (config.ticker){
+              return RequestService.getCompanyByTicker(config.ticker);
+            } //multiple api call use 
+          }]//http://www.martin-brennan.com/using-q-all-to-resolve-multiple-promises/
+        },
+        //edit: null
         edit: {
-          controller: 'RequestController',
-          templateUrl: '{widgetsPath}/adf-widget-investments/src/edit.html'
+           //controller: 'RequestController',
+           templateUrl: '{widgetsPath}/adf-widget-investments/src/edit.html'
         }
       }); 
   }]);
-angular.module("adf.widget.investments").run(["$templateCache", function($templateCache) {$templateCache.put("{widgetsPath}/adf-widget-investments/src/edit.html","<div><form role=form><div class=form-group><label for=sample>Gather information about your investements {{test}}</label> <input type=text class=form-control id=sample ng-model=ticker placeholder=\"Enter Company Ticker\"> <button ng-click=getCompanyByTicker() class=\"btn btn-primary\">Get Info!</button></div></form><div><h1>{{investment.name}}</h1></div></div>");
-$templateCache.put("{widgetsPath}/adf-widget-investments/src/view.html","<div ng-controller=RequestController><div ng-if=investment><h5>{{investment.name.substr(0,investment.name.indexOf(\'(\'))}}</h5><p>As of {{investment.data[0][0] | date:\'dd/MM/yyyy\'}} {{investment.dataset_code}} closed at a price of {{investment.data[0][11] | currency}} and had a volume of {{investment.data[0][12] | number:0}}.</p></div><form role=form><div class=form-group><input type=text class=form-control id=sample ng-model=ticker placeholder=\"Enter Company Name or Ticker\"> <button ng-click=getCompanyByTicker() class=\"btn btn-primary\">Get Info!</button></div></form></div>");}]);
+angular.module("adf.widget.investments").run(["$templateCache", function($templateCache) {$templateCache.put("{widgetsPath}/adf-widget-investments/src/edit.html","<div><form role=form><div class=form-group><label for=sample>Gather information about your investements</label> <input type=text class=form-control id=sample ng-model=config.ticker placeholder=\"Enter Company Ticker\"></div></form></div>");
+$templateCache.put("{widgetsPath}/adf-widget-investments/src/view.html","<div><div ng-if=investment><h5>{{investment.name.substr(0,investment.name.indexOf(\'(\'))}}</h5><p>As of {{investment.data[0][0] | date:\'dd/MM/yyyy\'}} {{investment.dataset_code}} closed at an adjusted price of {{investment.data[0][11] | currency}} and had a volume of {{investment.data[0][12] | number:0}}.</p></div></div>");}]);
 var app = angular.module('adf.widget.investments');
 app.controller('RequestController', RequestController);
 
 
-function RequestController(RequestService, $scope){
-	$scope.getCompanyByTicker = function(){
-    	RequestService.getCompanyByTicker($scope.ticker).then(function(data){
-			$scope.investment = data;
-			console.log(data);
-		});      
-	}
+function RequestController($scope, data) {
+    // $scope.getCompanyByTicker = function () {
+    //     RequestService.getCompanyByTicker($scope.ticker).then(function (data) {
+             $scope.investment = data;
+             if(data){
+                updateTitle();
+             }
+              //     });
+    // }
+    
+     function updateTitle() {
+        $scope.$parent.model.title = "Investment Analyzer - " + $scope.investment.dataset_code;;
+     }
 }
-RequestController.$inject = ["RequestService", "$scope"];
+RequestController.$inject = ["$scope", "data"];
+
+
 
 var app = angular.module('adf.widget.investments');
 app.service('RequestService', RequestService)
--- adf/adf-widget-investments/src/request-controller.js
+++ adf/adf-widget-investments/src/request-controller.js
@@ -2,11 +2,19 @@ var app = angular.module('adf.widget.investments');
 app.controller('RequestController', RequestController);
 
 
-function RequestController(RequestService, $scope){
-	$scope.getCompanyByTicker = function(){
-    	RequestService.getCompanyByTicker($scope.ticker).then(function(data){
-			$scope.investment = data;
-			console.log(data);
-		});      
-	}
+function RequestController($scope, data) {
+    // $scope.getCompanyByTicker = function () {
+    //     RequestService.getCompanyByTicker($scope.ticker).then(function (data) {
+             $scope.investment = data;
+             if(data){
+                updateTitle();
+             }
+              //     });
+    // }
+    
+     function updateTitle() {
+        $scope.$parent.model.title = "Investment Analyzer - " + $scope.investment.dataset_code;;
+     }
 }
+
+
